ef8cb2765427bdaf52cbbfec57e09c439246e93b
Abidu1 <abidusra@gmail.com>
2020-08-10 17:01:19 -0400

    setting up project

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Welcome to Makepin!</h1>
+        <p>This website is designed to create makeup boards with all your favourite products!</p>
+      </header>
+    )
+  }
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+ 
+class Header extends Component {
+    render() {
+      return (
+        <main>
+           <section class="blushSection">
+//         //     <div class="wrapper">
+//         //         <h2>Face Products</h2>
+//         //     </div>
+//         //     </section>
+    
+//         //     <section class="eyeSection">
+//         //     <div class="wrapper">
+//         //         <h2>Eye Products</h2>
+//         //     </div>
+//         //     </section>
+    
+//         //     <section class="lipSection">
+//         //     <div class="wrapper">
+//         //         <h2>Lip Products</h2>
+//         //     </div>
+//         //     </section>
+    
+//         //     <section class="nailSection">
+//         //     <div class="wrapper">
+//         //         <h2>Nail Products</h2>
+//         //     </div>
+//              </section>
+        </main>
+      )
+    }
+  }
+
+// class Main extends Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             product: {}
+//         }
+
+
+//     componentDidMount() {
+//         axios({
+//             method: 'get',
+//             url: 'http://makeup-api.herokuapp.com/api/v1/products.json',
+//             headers: { }
+//           }
+//         ).then(res => {
+//               this.setState({
+//             product: res.data,
+//               })
+//             //   .catch(function (error) {
+//             //     console.log(error);
+//         })
+    
+//     }
+// }
+
+// render() {
+//     return (
+//         // <main>
+//         //    
+//         // </main>
+//         )
+//     }
+
+//     }
+
+
+
+
+
+export default Main;
