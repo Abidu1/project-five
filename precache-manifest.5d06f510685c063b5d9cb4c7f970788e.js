24ee0fde6536a737c275821b0df21c5db15a4735
Abidu1 <abidusra@gmail.com>
2020-08-10 16:41:44 -0400

    Updates

---
+++ precache-manifest.5d06f510685c063b5d9cb4c7f970788e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf32be148123813a544bb178dbda917",
+    "url": "/project-five/index.html"
+  },
+  {
+    "revision": "b9f01a2d271dda73c458",
+    "url": "/project-five/static/css/main.58af5368.chunk.css"
+  },
+  {
+    "revision": "50c3f9b8cd22907e80a8",
+    "url": "/project-five/static/js/2.aeb26b55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-five/static/js/2.aeb26b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f01a2d271dda73c458",
+    "url": "/project-five/static/js/main.f914f456.chunk.js"
+  },
+  {
+    "revision": "8db48616468dfd325396",
+    "url": "/project-five/static/js/runtime-main.99782b0c.js"
+  }
+]);
