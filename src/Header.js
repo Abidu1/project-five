562ef8877b157b491b0c57c8c66507234bbab1bf
Abidu1 <abidusra@gmail.com>
2020-08-13 16:09:37 -0400

    changed description of website

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     return (
       <header>
         <h1>Welcome to Makepin!</h1>
-        <p>This website is designed to create makeup boards with all your favourite products!</p>
+        <p>This website is designed to Help you find makeup products!</p>
       </header>
     )
   }
